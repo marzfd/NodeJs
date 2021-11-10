81cf2dd2bf931c0ae8df956303149f9a6c2040e7
Marzieh <mar.31069@gmail.com>
2021-11-10 18:07:42 +0330

    Add weather route

--- week1/HackYourTemperature I/server.js
+++ week1/HackYourTemperature I/server.js
@@ -1,11 +1,23 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+
+const app = express();
 const port = 3000
 
+// Body parser middleware
+app.use(express.json());
+
+// Index Route
 app.get('/', (req, res) => {
   res.send('Hello from Backend to Frontend !')
 })
 
+// Weather Route
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(`The weather in ${cityName} is sunny`);
+})
+
+// Start Server
 app.listen(port, () =>
   console.log(`Server is running on port: ${port}`)
 )
